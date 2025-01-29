bda4437e143418b692072b18afe1d0ef2b7e1039
Leon Aquitaine <leonaquitaine@gmail.com>
2025-01-28 20:03:48 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+angular.module('BundlingwaysBestApp', [])
+    .controller('PresetController', function($scope, $http) {
+        $scope.year = new Date().getFullYear();
+
+        $http.get('presets.json')
+            .then(function(response) {
+                $scope.presets = response.data;
+            })
+            .catch(function(error) {
+                console.error('Error loading presets:', error);
+                $scope.presets = [
+                    {
+                        "name": "Error loading presets",
+                        "author": "Bundlingway",
+                        "instructions": "Please check the console for more information, and that presets.json is present and has the right content."
+                    }
+                ]
+            });
+    });

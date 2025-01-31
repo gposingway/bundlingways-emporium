86f7618479c9ea397dcc8d8ea7eb632922e1683c
Leo Botinelly <leonaquitaine@gmail.com>
2025-01-30 22:25:46 -0500

    Moving stuff around.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.UseFileServer();
+app.UseFileServer(new FileServerOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "docs")),
+    EnableDefaultFiles = true
+});
 
 app.Run();
